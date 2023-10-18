c763c5520b38fcc92ea050f9ec8ae9eb35b8180b
CoderWorld <134703565+Saurabh-Shrivastav@users.noreply.github.com>
2023-10-18 16:42:23 +0530

    Commited

---
+++ script.js
@@ -0,0 +1,17 @@
+function speak() {
+    const utter = new SpeechSynthesisUtterance();
+    console.log(utter);
+    utter.text = document.querySelector('#txt').value
+    speechSynthesis.speak(utter)
+    
+    // utter.voice = speechSynthesis.getVoices()[0]
+    // utter.pitch = 1
+    // utter.rate = 1
+    // utter.volume = 1
+    // utter.rate = 1.0 //speed 0.1-10
+    // utter.volume = 1.0 //0-1
+    // utter.pitch = 0.5 // 0 (low) - 2 (high)
+    // utter.voice = speechSynthesis.getVoices()[0];
+    // utter.text = document.querySelector('#txt').value;
+    // speechSynthesis.speak(utter)
+}
